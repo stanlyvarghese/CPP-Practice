acf6eb8911a2b088cf0533856107a5522f176cce
Stanly Varghese <120593144+stanlyvarghese@users.noreply.github.com>
2023-09-02 18:36:16 +0530

    Add files via upload

---
+++ patterns/patterns 3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin>>n;
+	
+	for(int i=1;i<=n;i++)
+	{
+		
+		for(int j=i;j<=i*2-1;j++)
+		{	
+		
+			cout<<j;
+			
+		}
+		cout<<endl;
+	}
+
+
+		
+}
